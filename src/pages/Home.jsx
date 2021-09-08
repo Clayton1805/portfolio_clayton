b314d1609e923803ca6f7ef81c20b649a18e5bc3
Clayton Miguel Da Silva <claytonmiguel1805@gmail.com>
2021-09-08 18:56:54 -0300

    primeiro commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+function Home() {
+  return (
+    <div>
+      <header>oi</header>
+      <div>apresentação</div>
+      <div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            legenda
+          </div>
+          <div>
+            <h2>titulo</h2>
+            descrição do projeto
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
