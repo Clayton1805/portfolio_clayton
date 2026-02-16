79036961fcfc680773d9b001b270da48702236cb
Clayton1805 <claytonmiguel1805@gmail.com>
2026-02-16 15:36:40 -0300

    atualiza imagens que virou um objeto ao inves de um array

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,38 +9,42 @@ const Div1Css = styled(motion.div)`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-
 `;
 
 function Portfolio() {
-  const {
-    gitConnected,
-  } = useContext(PortfolioContext);
+  const { gitConnected } = useContext(PortfolioContext);
 
   useEffect(() => window.scrollTo(0, 0), []);
 
   return (
     <BodyHeader imageClayton redes>
-
       {gitConnected && (
-      <Div1Css
-        variants={{
-          visible: {
-            transition: {
-              delayChildren: 0.5,
-              staggerChildren: 0.2,
+        <Div1Css
+          variants={{
+            visible: {
+              transition: {
+                delayChildren: 0.5,
+                staggerChildren: 0.2,
+              },
             },
-          },
-        }}
-        // transition={{ duration: 0.25 }}
-        initial="hidden"
-        animate="visible"
-      >
-        {gitConnected.projects
-          .map((project) => (
-            <CardProject key={project.name} project={project} />
-          ))}
-      </Div1Css>
+          }}
+          // transition={{ duration: 0.25 }}
+          initial="hidden"
+          animate="visible"
+        >
+          {gitConnected.projects.map((project) => {
+            console.log('project', project);
+            return (
+              <CardProject
+                key={project.name}
+                project={({
+                  ...project,
+                  images: Object.keys(project.images).map((key) => project.images[key]) || [],
+                })}
+              />
+            );
+          })}
+        </Div1Css>
       )}
     </BodyHeader>
   );
